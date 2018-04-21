b842b50b0049533b5fcf01a309df2744308d4a89
The Kwek-Kwek Kid <kwekkwekkid@gmail.com>
2018-04-21 22:34:31 +0800

    added function definitions

--- app.py
+++ app.py
@@ -2,10 +2,37 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
+BASE_DIR = "/api/companies"
+
 
 @app.route("/")
 def index():
     return "test"
+
+
+@app.route(BASE_DIR, methods = ["GET"])
+def get_companies():
+    return ""
+
+
+@app.route(BASE_DIR + "/<company_name>", methods = ["GET"])
+def get_company(company_name):
+    return ""
+
+
+@app.route(BASE_DIR, methods = ["POST"])
+def add_company():
+    return ""
+
+
+@app.route(BASE_DIR + "/<company_name>", methods = ["PUT"])
+def update_company(company_name):
+    return ""
+
+
+@app.route(BASE_DIR + "/<company_name>", methods = ["DELETE"])
+def remove_company(company_name):
+	return ""
 
 
 if(__name__ == "__main__"):
